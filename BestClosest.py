1ac06a55af0ce4a38b877ba681cc895bac0ce608
IlyaSolomatin <Locab.rus@gmail.com>
2018-02-01 02:16:04 +0300

    Improvements

--- BestClosest.py
+++ BestClosest.py
@@ -1,14 +1,8 @@
-from config import ALL_FILES, Get_FILES#,Get_FILES_Cat
+from config import ALL_FILES
 import pickle
 import numpy as np
 
-META_FILES = [ALL_FILES[i][:-5] for i in range(len(ALL_FILES)) if
-              i not in [91, 117, 137, 144, 328, 414, 423, 279, 129, 451]]
-
-#Uncomment the line with Get_FILES_Cat() and comment the line with Get_FILES() in order to work with files which are
-#computed with CatBoost, not SVM
-# FILES = Get_FILES_Cat()
-FILES = Get_FILES()
+FILES = ALL_FILES
 
 best_closest_qualities = []
 
@@ -16,22 +10,25 @@ best_closest_qualities = []
 #Pay attention to the directory you read the files from
 all_files = []
 for file in FILES:
-    all_files.append(np.loadtxt('./SVMgammaC/'+file+'.txt',delimiter=', '))
+    all_files.append(np.loadtxt('./SVMgammaC/'+file[:-5]+'.txt',delimiter=', '))
+    #Change the line above to the line below if you work with files which include
+    #precomputed time of execution (like XGBonGrid) in the last column
+    #all_files.append(np.loadtxt('./XGBonGrid/' + file[:-5] + '.txt', delimiter=', ')[:,:-1])
     if np.max(all_files[-1][:,-1]) != np.min(all_files[-1][:,-1]):
         all_files[-1][:,-1] = (all_files[-1][:,-1] - np.min(all_files[-1][:,-1]))  / (np.max(all_files[-1][:,-1])-np.min(all_files[-1][:,-1]))
     else:
-        all_files[-1][:,-1] = np.zeros_like(all_files[-1][:,-1])
+        all_files[-1][:,-1] = np.ones_like(all_files[-1][:,-1])
 all_files = np.array(all_files)
 
 #Now we load the matrix with metafeature vectors for all files
-A = np.array(pickle.load(open("A2.p","rb")))
+A = np.array(pickle.load(open("descriptive_vectors.p","rb")))
 
 for test_file in range(len(FILES)):
     closest_file = None
     closest_distance = np.inf
     for file in range(len(FILES)):
         if file != test_file:
-            dist = np.linalg.norm(np.array(A[META_FILES.index(FILES[test_file])])-np.array(A[META_FILES.index(FILES[file])]),ord=2)
+            dist = np.linalg.norm(np.array(A[test_file])-np.array(A[file]),ord=2)
             if dist < closest_distance:
                 closest_distance = dist
                 closest_dataset = file
